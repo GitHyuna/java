1483405d8431ccb52279c32e9e88e66296fe0d8b
Janghyuna <janghyuna7@gmail.com>
2022-08-02 05:18:16 +0900

    java Exam2022.08.02

---
+++ src/main/java/Hyuna/hellospring/java/_10_printf.java
@@ -0,0 +1,11 @@
+package Hyuna.hellospring.java;
+
+public class _10_printf {
+    //println : 변수의 값을 그대로 출력(값을 변환하지 않고는 다른 형식으로 출력할 수 없다.)
+
+    //printf : 변수의 값을 다른 형식으로 출력할 수 있다.
+    //지시자(specifier)를 통해 변수의 값을 여러 형식으로 변환하여 출력하는 기능을 가지고 있다.
+
+
+
+}
